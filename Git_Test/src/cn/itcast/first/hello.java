a86c2c4d1fdb8dffb6615682c99691fd605c6d41
username <username@qq.com>
2019-06-17 16:46:40 +0800

    把本地工程添加到本地仓库中

---
+++ Git_Test/src/cn/itcast/first/hello.java
@@ -0,0 +1,15 @@
+package cn.itcast.first;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        int i = 10;
+        i += 10;
+    }
+}
